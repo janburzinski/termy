6b682cf5c3423d7499065c59cc3fb7b8f79f1063
lasse <77295879+lassejlv@users.noreply.github.com>
2026-03-06 00:13:23 +0100

    Delete docs/examples/plugins/hello-rust/target directory

--- docs/examples/plugins/hello-rust/target/debug/deps/hello_rust_plugin-9d4284423d5d863a.d
+++
@@ -1,5 +0,0 @@
-/Users/lassevestergaard/Documents/dev/termy/docs/examples/plugins/hello-rust/target/debug/deps/hello_rust_plugin-9d4284423d5d863a.d: src/main.rs
-
-/Users/lassevestergaard/Documents/dev/termy/docs/examples/plugins/hello-rust/target/debug/deps/hello_rust_plugin-9d4284423d5d863a: src/main.rs
-
-src/main.rs:
